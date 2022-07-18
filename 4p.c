a37fdbfaa1c10c0adb1af0d26e66e919cb1fafc2
balucheeli <balu.cheeli@thundersoft.com>
2022-07-18 18:44:52 +0530

    pattern set-1 4question

---
+++ 4p.c
@@ -0,0 +1,30 @@
+/*
+ * 1
+ * 12
+ * 123
+ * 1234
+ * 12345
+ *
+ */
+
+#include<stdio.h>
+
+int main()
+{
+int i,j,n;
+printf("enter the no of lines\n");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+for(j=1;j<=i;j++)
+{
+printf("%d ",j);
+
+}
+printf("\n");
+
+}
+return 0;
+}
+
+
