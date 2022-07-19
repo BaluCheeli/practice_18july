faef11498f38570e6446d8ae0c40fe683f91f782
balucheeli <balu.cheeli@thundersoft.com>
2022-07-19 21:27:45 +0530

    pattern set-1 8question

---
+++ 8p.c
@@ -0,0 +1,22 @@
+/*
+1
+23
+456
+78910
+*/
+#include<stdio.h>
+int k=0;
+int main()
+{
+        int i,j,n;
+        printf("enter the no of rows\n");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+                for(j=i;j>0;j--)
+		{k=++k;
+                        printf("%d ",k);
+		}
+        printf("\n");
+        }
+}
