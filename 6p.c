856109dbf574fb8225f0b838e0fb8b87b5d9b432
balucheeli <balu.cheeli@thundersoft.com>
2022-07-19 21:20:43 +0530

    pattern set-1 6 question

---
+++ 6p.c
@@ -0,0 +1,21 @@
+/*
+5
+54
+543
+5432
+54321
+*/
+
+#include<stdio.h>
+int main()
+{
+        int i,j,n;
+        printf("Enter the no of rows\n");
+        scanf("%d",&n);
+        for(i=n;i>0;i--)
+        {
+                for(j=n;j>=i;j--)
+                        printf("%d",j);
+        printf("\n");
+        }
+}
