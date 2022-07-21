c4401a478b3a1b924defb56bc1ebcdf877458318
balucheeli <balu.cheeli@thundersoft.com>
2022-07-22 00:06:58 +0530

    pattern set-1 18question

---
+++ 18p.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int k=0;
+int main()
+{
+        int i,j,n;
+        printf("enter the no of rows\n");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+                for(j=i;j>0;j--)
+		{k=++k;
+                        printf("%c ",96+k);
+		}
+        printf("\n");
+        }
+}
